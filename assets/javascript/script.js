7ac8161a29a5d4301004c6a146bcc9894dde0278
Cloud <m.melanson000@gmail.com>
2022-07-14 20:05:32 -0400

    added logic to use our own button, to center map, and call restroom api results based on new location

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,48 @@
 let map, infoWindow;
+var gottaGo = document.querySelector('#custom-map-control-button');
 
+// calls restroom API data
+function getRestroomAPI(lat, lon) {
+  fetch('https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=5&offset=0&lat=' + lat + '&lng=' + lon)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+}
+// loads map and calls location of first public bathroom
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 51.4197, lng: 0.0831 },
     zoom: 15,
   });
   infoWindow = new google.maps.InfoWindow();
+}
 
-  const locationButton = document.createElement("button");
+function centerMap() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
 
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
+        getRestroomAPI(position.coords.latitude, position.coords.longitude);
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Location found.");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -48,14 +57,4 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 window.initMap = initMap;
 
-// function getRestroomAPI (lat, lon) {
-//     fetch ('https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=5&offset=0&lat=' + lat + '&lng=' + lon)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//     })
-// }
-
-// getRestroomAPI(45, -70);
+gottaGo.addEventListener('click', centerMap);
